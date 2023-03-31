47aadd335bc9b2a4f291bf8b53d87ab07fb1b3c2
weslley <weslleysouza98433@gmail.com>
2023-03-31 03:30:11 -0300

    fabrica de carros

---
+++ Desafios/src/fabrica-de-carros/desafio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class desafio1{
+
+	public static void main(String[] args) {
+
+	 Scanner scan = new Scanner(System.in);
+     Double custoFabrica = scan.nextDouble();
+     Double porcentagemDistribuidor = scan.nextDouble();
+     Double percentualImpostos = scan.nextDouble();
+     
+     Double valorDistribuidor = custoFabrica * (porcentagemDistribuidor / 100);
+
+     Double valorImpostos = custoFabrica * (percentualImpostos / 100);
+
+     Double precoFinal = custoFabrica + valorDistribuidor + valorImpostos;
+
+     System.out.println(String.format("%.0f", precoFinal));
+ 
+    }
+}  
