1f2a8bcde1a1b50a99d15ae3ebc1d659fca7490e
weslley <weslleysouza98433@gmail.com>
2023-04-04 03:48:08 -0300

    Desafios da DIO

--- Desafios/src/imprimindo-inteiros-e-media/desafio2.java
+++ Desafios/src/imprimindo-inteiros-e-media/desafio2.java
@@ -4,14 +4,14 @@ public class desafio2 {
 	
   public static void main(String[] args) {
 
-     Scanner leitor = new Scanner(System.in);
+     Scanner scan = new Scanner(System.in);
      int cont = 0;
      double media = 0;
      double x;
 
         for (int i = 0; i <6 ; i ++) { 
-         x = leitor.nextDouble();
-         i = i;
+         x = scan.nextDouble();
+         i += i;
 
           if (  x > 0) {
             cont++;
